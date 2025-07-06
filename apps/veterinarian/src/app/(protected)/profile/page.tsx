a1dd326b6297b6b909120b2f2facfc5f183ce30f
yannjez <yannjez@gmail.com>
2025-07-06 18:37:24 +0200

    Remove unused structure fetching logic from ProfilePage component

--- apps/veterinarian/src/app/(protected)/profile/page.tsx
+++ apps/veterinarian/src/app/(protected)/profile/page.tsx
@@ -1,8 +1,5 @@
 import ProfilePageContent from '@/components/clerk/ProfilePageContent';
-import { getStructureById } from './profileController';
 
 export default async function ProfilePage() {
-  const structure = await getStructureById('1');
-  console.log(structure);
   return <ProfilePageContent />;
 }
