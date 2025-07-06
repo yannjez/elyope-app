ed3ccf1ae85140028996838dd11a7f8fb976a798
yann-j-nomiks <yannj@nomiks.io>
2025-07-06 13:30:30 +0200

    Refactor Next.js configuration for improved plugin integration
    - Simplified the Next.js configuration by directly composing plugins with `withNx` and `withNextIntl`.
    - Removed unnecessary comments and cleaned up the code for better readability.

--- apps/veterinarian/next.config.js
+++ apps/veterinarian/next.config.js
@@ -1,6 +1,5 @@
 //@ts-check
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
 const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
@@ -9,24 +8,14 @@ const withNextIntl = createNextIntlPlugin();
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  // Use this to set Nx-specific options
-  // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
   webpack: (config) => {
-    // Configure SVGR manually
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
-
     return config;
   },
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-  withNextIntl,
-];
-
-module.exports = composePlugins(...plugins)(nextConfig);
+module.exports = composePlugins(withNx, withNextIntl)(nextConfig);
